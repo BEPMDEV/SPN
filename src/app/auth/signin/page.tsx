441b906f5671e40dcb4ed1cbfffc52afe95fdab2
BEPMDEV <andybekhampm@gmail.com>
2024-07-14 23:32:51 -0500

    modificaciones al login

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,40 +1,37 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function SignInPage() {
   return (
 
     <section className="min-h-screen flex items-stretch text-white ">
-      <div className="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80")' }}>
-        <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+      <div className="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center" style={{ backgroundImage: 'url(/images/login/background.jpg)' }}>
+        <div className="absolute bg-orange-950 opacity-50 inset-0 z-0"></div>
         <div className="w-full px-24 z-10">
-          <h1 className="text-5xl font-bold text-left tracking-wide">SPN</h1>
-          <p className="text-3xl my-4">Sistema de Padron Nominal...</p>
+          <h1 className="text-5xl text-orange-50 font-bold text-left tracking-wide">SISTEMA DE PADRÓN NOMINAL DE GESTANTES</h1>
         </div>
       </div>
-      <div className="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0" style={{ backgroundColor: '#161616' }}>
-        <div className="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80")' }}>
-          <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+      <div className="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0 bg-orange-50">
+        <div className="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center" style={{ backgroundImage: 'url(/images/login/background.jpg)' }}>
+          <div className="absolute bg-orange-950 opacity-50 inset-0 z-0"></div>
         </div>
         <div className="w-full py-6 z-20">
-          <h1 className="my-6">
-            <svg viewBox="0 0 50 31" className="h-7 sm:h-8 mx-auto">
-              <path className="" fill="rgba(99,102,241, .8)" fillRule="evenodd" clipRule="evenodd" d="M25.517 0C18.712 0 14.46 3.382 12.758 10.146c2.552-3.382 5.529-4.65 8.931-3.805 1.941.482 3.329 1.882 4.864 3.432 2.502 2.524 5.398 5.445 11.722 5.445 6.804 0 11.057-3.382 12.758-10.145-2.551 3.382-5.528 4.65-8.93 3.804-1.942-.482-3.33-1.882-4.865-3.431C34.736 2.92 31.841 0 25.517 0zM12.758 15.218C5.954 15.218 1.701 18.6 0 25.364c2.552-3.382 5.529-4.65 8.93-3.805 1.942.482 3.33 1.882 4.865 3.432 2.502 2.524 5.397 5.445 11.722 5.445 6.804 0 11.057-3.381 12.758-10.145-2.552 3.382-5.529 4.65-8.931 3.805-1.941-.483-3.329-1.883-4.864-3.432-2.502-2.524-5.398-5.446-11.722-5.446z" >
-              </path>
-            </svg>
+          <h1 className="w-full">
+            <Image className="mx-auto" src={'/images/login/logo.png'} width={150} height={150} alt="logo"/>
           </h1>
 
           <form action="" className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
             <div className="pb-2 pt-4">
-              <input type="email" name="email" id="email" placeholder="Usuario" className="block w-full p-4 text-lg rounded-sm bg-black" />
+              <input type="email" name="email" id="email" placeholder="Ingrese Usuario" className="block lg:bg-transparent text-black w-full p-4 text-lg rounded-md placeholder-orange-600 focus:outline-orange-500 border-2 border-orange-500" />
             </div>
             <div className="pb-2 pt-4">
-              <input className="block w-full p-4 text-lg rounded-sm bg-black" type="password" name="password" id="password" placeholder="Contraseña" />
+              <input className="block text-black w-full lg:bg-transparent p-4 text-lg rounded-md placeholder-orange-600 focus:outline-orange-500 border-2 border-orange-500" type="password" name="password" id="password" placeholder="Ingrese Contraseña" />
             </div>
-            <div className="text-right text-gray-400 hover:underline hover:text-gray-100">
+            {/*<div className="text-right text-black hover:underline">
               <a href="#">Olvidastes tu contraseña?</a>
-            </div>
+            </div>*/}
             <div className="px-4 pb-2 pt-4">
-              <Link href={'/home'} className="uppercase block w-full p-4 text-lg rounded-full bg-indigo-500 hover:bg-indigo-600 focus:outline-none">Iniciar Sesión</Link>
+              <Link href={'/home'} className="uppercase block w-full p-4 text-lg rounded-full bg-orange-600 hover:bg-orange-500 focus:outline-none transition-colors duration-300 font-bold">Iniciar Sesión</Link>
             </div>
           </form>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title:
-    "Dasboard | SPN"
+    "Dashboard | SPN"
 };
 
 export default function Home() {
